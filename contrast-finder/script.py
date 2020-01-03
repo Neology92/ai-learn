2adffbf88dff61fc3909c613fcd15975f7a7ff9d
Neology92 <oskarlegner@gmail.com>
2020-01-03 17:24:57 +0100

    Get synaptic weights

--- contrast-finder/script.py
+++ contrast-finder/script.py
@@ -1,5 +1,14 @@
 from create_sets import read_from_files
+import numpy as np
+import random as rnd
 
-X_train, Y_train, X_test, Y_test = read_from_files("./data")
 
-print(X_train.__len__(), Y_train.__len__(), X_test.__len__(), Y_test.__len__())
+def sigmoid(x):
+    return 1 / (1 + np.e**(-x))
+
+
+# X_train, Y_train, X_test, Y_test = read_from_files("./data")
+
+synaptic_weights = 2 * np.random.random((3)) - 1
+
+print (synaptic_weights)
