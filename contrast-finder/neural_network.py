679ebbd60891f1da2f77bfe823ce5cc3f77e2ca0
Neology92 <oskarlegner@gmail.com>
2020-01-07 17:10:52 +0100

    Add training method

--- contrast-finder/neural_network.py
+++ contrast-finder/neural_network.py
@@ -6,10 +6,10 @@ class NeuralNetwork():
     neurons = []
     weights = []    
     bias = []    
-    LEARNING_RATE = 0.1
+    LEARNING_RATE = 1
+    BIAS_LEARNING_RATE = 1
 
     def __init__(self, neurons = [1,1]):
-        
         # Check if neurons have right configuration
         self.neurons  = list(map(int, neurons))
         self.layers_number  = neurons.__len__()
@@ -28,6 +28,7 @@ class NeuralNetwork():
             self.weights.append( 2 * np.random.random(( self.neurons[i], self.neurons[i+1] )) - 1 )
             self.bias.append( 2 * np.random.random(( 1, self.neurons[i+1] )) - 1 )
         
+
     def feedforward(self, input_arr):
         neurons_values = []
         neurons_values.append(input_arr)
@@ -38,13 +39,59 @@ class NeuralNetwork():
             neurons_values.append( self.sigmoid(next_neurons_sums) )
 
         return neurons_values[-1]
+        
+
+    def train(self, train_X, train_Y, times):
+        for t in range(times):
+
+            # Forward propagation
+            # -------------------
+            neurons_values = []
+            neurons_values.append(train_X)
+            for i in range(0, self.layers_number-1):
+                # Calculate sum
+                next_neurons_sums = np.dot(neurons_values[i], self.weights[i]) + self.bias[i]
+                # Activation function
+                neurons_values.append( self.sigmoid(next_neurons_sums) )
+            output = neurons_values[-1]
+
+            cost = np.sum((output - train_Y)**2)
+            print("\nCost: ")
+            print(cost)
+            # print("\nWeights:")
+            # print(self.weights[-1])
+            # print("\nBias:")
+            # print(self.bias[-1])
+            
+            # Back propagation
+            # -------------------
+            errors = [0] * (self.layers_number-1)
+            # Calc errors
+            errors[-1] = train_Y - output
+            for i in range(2, self.layers_number):
+                errors[-i] = np.dot(errors[-(i-1)], self.weights[-(i-1)].T )
+
+            # Calc new weights and bias
+            for i in range(1, errors.__len__()+1):
+                gradient = errors[i-1] * self.d_sigmoid(neurons_values[i])
+                
+                weights_delta = self.LEARNING_RATE * np.dot(neurons_values[i-1].T, gradient)
+                gradient_rows = np.ma.size(gradient, 0)
+                bias_delta = self.BIAS_LEARNING_RATE * np.dot( np.ones((1,gradient_rows)) , gradient)
+                
+                # Update weights and bias
+                self.weights[i-1] += weights_delta
+                self.bias[i-1] += bias_delta
+            # end_for
+        # end_for
+        
 
     def sigmoid(self, x):
         return 1.0 / (1 + np.exp(-x))
 
-    # Gets sigmoid function as input
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    # Gets sigmoid output as input
+    def d_sigmoid(self, y):
+        return y * (1 - y)
 
 
 
--- contrast-finder/script.py
+++ contrast-finder/script.py
@@ -1,8 +1,28 @@
 from neural_network import NeuralNetwork
+import numpy as np
 
 if __name__ == "__main__":
 
-    neural_network = NeuralNetwork([1])
+    neural_network = NeuralNetwork([3,2,1])
 
-    output = neural_network.feedforward([1])
-    print(output)
+    # input_arr = np.array([[1,0],[0,1]])
+    # output = neural_network.feedforward(input_arr)
+    # print(output)
+
+    train_X = np.array([[1,0,0],
+                        [1,1,1],
+                        [0,1,1],
+                        [0,1,0]])
+
+    train_Y = np.array([[1],
+                        [1],
+                        [0],
+                        [0]])
+
+    neural_network.train(train_X, train_Y, 100)
+
+    while(True):
+        a = list(map( int, input("Get array[3]: ") ))
+
+        output = neural_network.feedforward(a)
+        print(output)
